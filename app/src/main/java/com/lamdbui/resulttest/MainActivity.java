eb6406bb820bdb52ff010cc1517b13e878ca26ff
Lam Bui <lamdbui@gmail.com>
2016-10-06 17:26:10 -0700

    Add second Activity and hooked it up

--- app/src/main/java/com/lamdbui/resulttest/MainActivity.java
+++ app/src/main/java/com/lamdbui/resulttest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lamdbui.resulttest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -27,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
         mItemCountTextView.setText(mItems.size() + " number of items");
 
         mStartTestButton = (Button) findViewById(R.id.start_test_button);
+        mStartTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ItemListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
